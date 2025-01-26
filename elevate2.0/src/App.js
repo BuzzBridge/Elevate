de2b2965cac69469e848f4121c5e6c67dc07de32
ganeshFalcon <ganesh@falconfs.com>
2025-01-26 21:39:16 +0530

    Add hero component

--- elevate2.0/src/App.js
+++ elevate2.0/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
 import Courses from './components/Courses/Courses';
 import CourseDetail from './components/Courses/CourseDetail';
 import Instructors from './components/Instructors/Instructors';
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={
             <div>
               <Navbar />
+              <Hero />
               <Courses />
             </div>
           } />
---
+++ elevate2.0/src/components/hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+    const mobileVideo = require(`./assets/videos/heroMobileBackground.mp4`)
+    const webVideo = require(`./assets/videos/heroWebBackground.mp4`)
+
+    return (
+        <div className='hero-section'>
+            <div className='hero-section-web' >
+                <video className="hero-video hero-video-large" autoPlay loop muted>
+                    <source src={webVideo} type="video/mp4" />
+                </video>
+            </div>
+            <div className='hero-section-mobile'>
+                <video className="hero-video hero-video-small" autoPlay loop muted>
+                    <source src={mobileVideo} type="video/mp4" />
+                </video>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
