8ad784694c0fbf72f877ca884e4bb50b2e4c15ff
ganeshFalcon <ganesh@falconfs.com>
2025-01-27 04:40:12 +0530

    Changes for github pages

--- elevate2.0/src/App.js
+++ elevate2.0/src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Elevate">
       <div className="App">
         <Routes>
           <Route path="/" element={
