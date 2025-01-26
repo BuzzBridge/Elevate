dd264493d342af9c35b68ba63f481bd245e6c142
ganeshFalcon <ganesh@falconfs.com>
2025-01-26 18:46:16 +0530

    Add navbar

--- elevate2.0/src/App.js
+++ elevate2.0/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 import Courses from './components/Courses/Courses';
 import CourseDetail from './components/Courses/CourseDetail';
 import './App.css';
@@ -9,7 +10,12 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Courses />} />
+          <Route path="/" element={
+            <div>
+              <Navbar />
+              <Courses />
+            </div>
+          } />
           <Route path="/course/:id" element={<CourseDetail />} />
         </Routes>
       </div>
