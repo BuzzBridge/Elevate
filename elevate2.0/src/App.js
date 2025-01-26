c1aea853eb36f0df503d9d704fdfdcd2ab3c1eae
ganeshFalcon <ganesh@falconfs.com>
2025-01-26 21:39:16 +0530

    Add hero component

--- elevate2.0/src/App.js
+++ elevate2.0/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
 import Courses from './components/Courses/Courses';
 import CourseDetail from './components/Courses/CourseDetail';
 import './App.css';
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={
             <div>
               <Navbar />
+              <Hero />
               <Courses />
             </div>
           } />
