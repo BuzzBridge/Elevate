8cd5c12fc0ccda9db2a760ff6b452dfc7c547031
pandeyGanesh <pandey.ganesh12@gmail.com>
2025-02-04 13:37:02 +0530

    Changes for gif file

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,8 +2,9 @@ import React, { useEffect } from "react";
 import './Hero.css';
 
 const Hero = () => {
-    const mobileVideo = require(`./assets/videos/heroMobileBackground.mp4`)
-    const webVideo = require(`./assets/videos/heroWebBackground.mp4`)
+    // const mobileVideo = require(`./assets/videos/heroMobileBackground.mp4`);
+    const mobileGif = require(`./assets/gif/heroMobileBackground.gif`);
+    const webVideo = require(`./assets/videos/heroWebBackground.mp4`);
 
     useEffect(() => {
         const fixViewportHeight = () => {
@@ -20,28 +21,33 @@ const Hero = () => {
     return (
         <div className='hero-section'>
             <div className='hero-section-web' >
-            <video 
-    className="hero-video" 
-    autoPlay 
-    loop 
-    muted 
-    playsInline /* Prevents iOS fullscreen mode */
->
-    <source src={require(`./assets/videos/heroWebBackground.mp4`)} type="video/mp4" />
-</video>
+                <video
+                    className="hero-video"
+                    autoPlay
+                    loop
+                    muted
+                    playsInline /* Prevents iOS fullscreen mode */
+                >
+                    <source src={webVideo} type="video/mp4" />
+                </video>
 
             </div>
             <div className='hero-section-mobile'>
-                <video className="hero-video hero-video-small" autoPlay loop muted>
+                <img
+                    className="hero-video hero-video-small"
+                    src={mobileGif} // Replace `mobileVideo` with the path to your GIF
+                    alt="Hero GIF"
+                />
+                {/* <video className="hero-video hero-video-small" autoPlay loop muted>
                     <source src={mobileVideo} type="video/mp4" />
-                </video>
+                </video> */}
             </div>
-            <button 
-    className='enroll-button' 
-    onClick={() => window.open("https://form.jotform.com/250181201079447", "_blank")}
->
-    <strong>Enroll Now</strong>
-</button>
+            <button
+                className='enroll-button'
+                onClick={() => window.open("https://form.jotform.com/250181201079447", "_blank")}
+            >
+                <strong>Enroll Now</strong>
+            </button>
 
         </div>
     );
